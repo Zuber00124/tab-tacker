e3197784cc67d778a59a1a86b0d0960f74370829
Zuber <zuber00124@gmail.com>
2017-09-13 21:51:41 +0530

    P3 Login Register Client Server

--- client/src/components/Login.vue
+++ client/src/components/Login.vue
@@ -3,7 +3,7 @@
     <v-flex xs6 offset-xs3>
       <div class="white elevation-2">
         <v-toolbar flat dense class="cyan" dark>
-          <v-toolbar-title>Register</v-toolbar-title>
+          <v-toolbar-title>Login</v-toolbar-title>
         </v-toolbar>
 
         <div class="pl-4 pr-4 pr-2 pb-2">
@@ -26,8 +26,8 @@
           <div class="error" v-html="error" />
 
           <v-btn
-            @click="register"  class="cyan">
-            Register
+            @click="login"  class="cyan">
+            Login
           </v-btn>
         </div>
       </div> 
@@ -48,9 +48,9 @@ export default {
     }
   },
   methods: {
-    async register () {
+    async login () {
       try {
-        await AuthenticationService.register({
+        await AuthenticationService.login({
           email: this.email,
           password: this.password
         })
--- server/src/controllers/AuthenticationController.js
+++ server/src/controllers/AuthenticationController.js
@@ -7,7 +7,38 @@ module.exports = {
       res.send(user.toJSON()) 
     } catch (err) {
       res.status(400).send({
-        error: 'This email account is already in use.'
+        error: 'This email hacked account is already in use.'
+      })
+    }    
+  },
+  async login (req, res) {
+    try {
+      const {email, password} = req.body
+      const user = await User.findOne({
+        where: {
+          email: email
+        }
+      })
+
+      if (!user) {
+        return res.status(403).send({
+          error: 'The login information was incorrect'
+        })
+      }
+      const isPasswordValid = password === user.password
+      
+      if (!isPasswordValid) {
+        return res.status(400).send({
+          error: 'The login information was incorrect'
+        })
+      }
+      const userJson = user.toJSON()
+      res.send({
+        user: userJson
+      }) 
+    } catch (err) {
+      res.status(500).send({
+        error: 'An error has occured trying to log in'
       })
     }    
   }  
